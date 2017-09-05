9ac9ebd99b9c354d2dba7423762b2fc4d17de08b
He-Dongwei <1710402410@qq.com>
2017-09-05 19:15:03 +0800

    Update admin.py

--- reg/artical/admin.py
+++ reg/artical/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import paper,journal,author,reviewer,web,review
+from .models import paper,journal,author,recommend,reviewer,web,review
 admin.site.register(paper)
 admin.site.register(journal)
+admin.site.register(recommend)
 admin.site.register(author)
 admin.site.register(reviewer)
 admin.site.register(web)
